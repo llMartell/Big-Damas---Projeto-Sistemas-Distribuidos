8caed70e1b75c668e31491d639daf6e0108d6bf7
DantonFontes <dantonleite2005@gmail.com>
2026-05-12 16:30:25 -0300

    adicionando controle de fim de partida e verificação de vencedor

--- big-damas-backend/src/main/java/br/bigdamas/service/JogoService.java
+++ big-damas-backend/src/main/java/br/bigdamas/service/JogoService.java
@@ -9,10 +9,12 @@ public class JogoService {
 
     private Tabuleiro tabuleiro;
     private Jogador jogadorAtual;
+    private boolean jogoEncerrado;
 
     public JogoService() {
         this.tabuleiro = new Tabuleiro();
         this.jogadorAtual = Jogador.JOGADOR_1;
+        this.jogoEncerrado = false;
     }
 
     public Tabuleiro getTabuleiro() {
@@ -23,29 +25,48 @@ public class JogoService {
         return jogadorAtual;
     }
 
+    public boolean isJogoEncerrado() {
+        return jogoEncerrado;
+    }
+
     public boolean moverPeca(int origemLinha, int origemColuna, int destinoLinha, int destinoColuna) {
+
+        if (jogoEncerrado) {
+
+            System.out.println("O jogo já terminou.");
+
+            return false;
+        }
 
         Peca[][] matriz = tabuleiro.getMatriz();
 
         if (origemLinha < 0 || origemLinha > 7 || origemColuna < 0 || origemColuna > 7 || destinoLinha < 0 || destinoLinha > 7 || destinoColuna < 0 || destinoColuna > 7) {
+
             System.out.println("Posição inválida.");
+
             return false;
         }
 
         if (matriz[origemLinha][origemColuna] == null) {
+
             System.out.println("Não existe peça nessa posição.");
+
             return false;
         }
 
         Peca peca = matriz[origemLinha][origemColuna];
 
         if (peca.getJogador() != jogadorAtual) {
+
             System.out.println("Não é a vez desse jogador.");
+
             return false;
         }
 
         if (matriz[destinoLinha][destinoColuna] != null) {
+
             System.out.println("Destino ocupado.");
+
             return false;
         }
 
@@ -57,17 +78,23 @@ public class JogoService {
         int direcao;
 
         if (peca.getJogador() == Jogador.JOGADOR_1) {
+
             direcao = 1;
+
         } else {
+
             direcao = -1;
         }
 
         if ((diferencaLinha == direcao || (ehDama && diferencaLinha == -direcao)) && Math.abs(diferencaColuna) == 1) {
 
             matriz[destinoLinha][destinoColuna] = peca;
+
             matriz[origemLinha][origemColuna] = null;
 
             verificarPromocao(peca, destinoLinha);
+
+            verificarVencedor();
 
             trocarTurno();
 
@@ -77,6 +104,7 @@ public class JogoService {
         if ((diferencaLinha == direcao * 2 || (ehDama && diferencaLinha == -direcao * 2)) && Math.abs(diferencaColuna) == 2) {
 
             int linhaMeio = (origemLinha + destinoLinha) / 2;
+
             int colunaMeio = (origemColuna + destinoColuna) / 2;
 
             Peca pecaMeio = matriz[linhaMeio][colunaMeio];
@@ -86,9 +114,12 @@ public class JogoService {
                 matriz[linhaMeio][colunaMeio] = null;
 
                 matriz[destinoLinha][destinoColuna] = peca;
+
                 matriz[origemLinha][origemColuna] = null;
 
                 verificarPromocao(peca, destinoLinha);
+
+                verificarVencedor();
 
                 trocarTurno();
 
@@ -97,6 +128,7 @@ public class JogoService {
         }
 
         System.out.println("Movimento inválido.");
+
         return false;
     }
 
@@ -104,7 +136,9 @@ public class JogoService {
 
         if (jogadorAtual == Jogador.JOGADOR_1) {
             jogadorAtual = Jogador.JOGADOR_2;
+
         } else {
+
             jogadorAtual = Jogador.JOGADOR_1;
         }
     }
@@ -112,11 +146,60 @@ public class JogoService {
     private void verificarPromocao(Peca peca, int linhaDestino) {
 
         if (peca.getJogador() == Jogador.JOGADOR_1 && linhaDestino == 7) {
+
             peca.virarDama();
         }
 
         if (peca.getJogador() == Jogador.JOGADOR_2 && linhaDestino == 0) {
+
             peca.virarDama();
+        }
+    }
+
+    private int contarPecas(Jogador jogador) {
+
+        int quantidade = 0;
+
+        Peca[][] matriz = tabuleiro.getMatriz();
+
+        for (int linha = 0; linha < 8; linha++) {
+
+            for (int coluna = 0; coluna < 8; coluna++) {
+
+                Peca peca = matriz[linha][coluna];
+
+                if (peca != null && peca.getJogador() == jogador) {
+
+                    quantidade++;
+                }
+            }
+        }
+
+        return quantidade;
+    }
+
+    private void verificarVencedor() {
+
+        int pecasJogador1 = contarPecas(Jogador.JOGADOR_1);
+
+        int pecasJogador2 = contarPecas(Jogador.JOGADOR_2);
+
+        if (pecasJogador1 == 0) {
+
+            jogoEncerrado = true;
+
+            System.out.println();
+            System.out.println("JOGADOR_2 venceu!");
+            System.out.println();
+        }
+
+        if (pecasJogador2 == 0) {
+
+            jogoEncerrado = true;
+
+            System.out.println();
+            System.out.println("JOGADOR_1 venceu!");
+            System.out.println();
         }
     }
 }
