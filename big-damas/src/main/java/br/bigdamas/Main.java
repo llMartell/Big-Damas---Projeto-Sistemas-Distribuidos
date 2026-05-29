a86db959f6cc1ecb5a49bc4f8986f3837a56f917
Marte <159561648+llMartell@users.noreply.github.com>
2026-05-29 11:03:23 -0300

    Add comments for server IP setup in Main.java
    Added comments for IP configuration instructions.

--- big-damas/src/main/java/br/bigdamas/Main.java
+++ big-damas/src/main/java/br/bigdamas/Main.java
@@ -12,6 +12,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		try {
+			//Alterar o IP sempre que for testar (coloque o IP da maquina que vai servir de servidor)
+			//Basta solicitar que o responsavel pelo servidor apresente a IP assim que rodor o servidor
 			String ipServidor = "10.8.185.20";
 			IJogoService jogoService = (IJogoService) Naming.lookup("rmi://" + ipServidor + ":1099/BigDamasService");
 			System.out.println("Cliente: Conectado com sucesso ao servidor RMI!");
